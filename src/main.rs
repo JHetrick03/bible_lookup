1a3e7f23a6888f0e8f41e71db3a5f64266f30e1a
David B. Moore <mtrunmoore@gmail.com>
2023-12-07 20:47:33 -0500

    Added the file printing functionality.

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,16 @@ use std::io::BufRead;
 use std::io::BufReader;
 use std::collections::HashMap;
 use std::io::Write;
-//use std::thread::yield_now;
+use std::fs::OpenOptions;
 
 fn main () {
-	// println!("Please enter a verse:");
-	// let mut verse = String::new()
-	// std::io::stdin().read_line(&mut verse).unwrap();
-	// println!("You entered {}", verse);
-
     let mut book_abbreviations: HashMap<String, String> = HashMap::new();
 
     let bible_abbreviations_filename: &str = "Bible_Abbreviations.csv";
     let file = File::open(bible_abbreviations_filename).expect("Failed to open file");
+
+    let mut verse_file = OpenOptions::new().write(true)
+    .open("verses.txt").expect("Failed to open verses.txt");
 
     let reader = BufReader::new(file);
 
@@ -28,8 +26,6 @@ fn main () {
     }
 
     loop {
-
-        // TODO: Continue Bible Lookup algorithm (see Python BL project for outline of what steps to do)
         println!("Please enter the reference of the verse you would like to retrieve");
         
         let mut book = String::new();
@@ -56,11 +52,9 @@ fn main () {
         
         let mut psalm = "PSALM ".to_owned() + &chapter;
         psalm = psalm.trim().to_owned();
-        //let psalm_len = psalm.len();
 
         chapter = "CHAPTER ".to_owned() + &chapter;
         chapter = chapter.trim().to_owned();
-        //let chap_len = chapter.len(); //I don't think this is ever read
 
         verse = verse.trim().to_owned();
         let line_verse = verse.len();
@@ -88,50 +82,46 @@ fn main () {
                 chap_found = true;
             }
             // psalm is found?
-            else if book_found && unwrapped == psalm { //Psalms 3:5 works, but Psalm 3:5 doesn't
+            else if book_found && unwrapped == psalm { 
                 chap_found = true;
             }
             
-            // else if book_found && unwrapped.len() == psalm_len {
-            //     if &unwrapped[..psalm_len] == psalm {
-            //         chap_found = true;
-            //     }
-            // }
-
             // checking for verse
             else if chap_found && &unwrapped[..line_verse] == verse {
                 verse_found = true;
                 println!("\nThe verse requested was:");
                 println!("{}", unwrapped);
+                write!(verse_file, "{}", unwrapped).expect("Error writing to verses.txt");
+                write!(verse_file,"\n").expect("Error writing to verses.txt");
                 //To do:
-                //Append the verse to verses.txt
                 //Format the terminal output to "pretty print" it
                 break;
             }
 
-            // Error cases
+            // Check for error cases
             else if book_found && !chap_found && unwrapped.len() >= 11 {
                 if &unwrapped[..11] == "THE BOOK OF" {
-                    println!("ERROR: This chapter does not exist!");
                     break;
                 }
             }
             else if chap_found && !verse_found && &unwrapped[..5] == "PSALM" {
-                println!("ERROR: This verse does not exist!");
                 break;
             }
             else if chap_found && !verse_found && &unwrapped[..7] == "CHAPTER" {
-                println!("ERROR: This verse does not exist!");
                 break;
             }
-            //Rev. 23:1 and Rev. 22:22 return NOTHING (create an error case for this)
         }
 
-        // Book error case
+        // Error messages
         if !book_found {
             println!("ERROR: This book does not exist!");
         }
-
+        else if !chap_found {
+            println!("ERROR: This chapter does not exist!");
+        }
+        else if !verse_found {
+            println!("ERROR: This verse does not exist!");
+        }
         
         // checking if do again
         println!("\nWould you like to continue? Y/N ");
